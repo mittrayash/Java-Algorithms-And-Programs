77956869ac0ab90bf0784387ad33501f19989383
mittrayash <mittrayash@gmail.com>
2017-12-06 20:52:24 +0530

    Scientific Calc in java update

--- ScientificCalc.java
+++ ScientificCalc.java
@@ -36,4 +36,30 @@ class Calculator extends JFrame {
                 buttonPanel.add(button);
             }
         }
-    
+        ActionListener operatorListener = new OperatorListener();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4, 4, 4));
+        String[] opOrder = {"+", "-", "*", "/","=","C","sin","cos","log"};
+        for (int i = 0; i < opOrder.length; i++) {
+            JButton button = new JButton(opOrder[i]);
+            button.addActionListener(operatorListener);
+            button.setFont(BIGGER_FONT);
+            panel.add(button);
+        }
+        JPanel pan = new JPanel();
+        pan.setLayout(new BorderLayout(4, 4));
+        pan.add(textfield, BorderLayout.NORTH );
+        pan.add(buttonPanel , BorderLayout.CENTER);
+        pan.add(panel , BorderLayout.EAST);
+        this.setContentPane(pan);
+        this.pack();
+        this.setTitle("Calculator");
+        this.setResizable(false);
+    }
+    private void action() {
+        number = true;
+        textfield.setText("");
+        equalOp = "=";
+        op.setTotal("");
+    }
+   
