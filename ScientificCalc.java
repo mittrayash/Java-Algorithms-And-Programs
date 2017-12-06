25e22c8b46b9e4a254f93edf7237b37b31d116e4
mittrayash <mittrayash@gmail.com>
2017-12-06 20:53:33 +0530

    Scientific calc update

--- ScientificCalc.java
+++ ScientificCalc.java
@@ -62,4 +62,27 @@ class Calculator extends JFrame {
         equalOp = "=";
         op.setTotal("");
     }
-   
+    class OperatorListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String displayText = textfield.getText();
+            if (e.getActionCommand().equals("sin"))
+            {
+                textfield.setText("" + Math.sin(Double.valueOf(displayText).doubleValue()));
+                
+            }else
+            if (e.getActionCommand().equals("cos"))
+            {
+                textfield.setText("" + Math.cos(Double.valueOf(displayText).doubleValue()));
+                
+            }
+            else
+            if (e.getActionCommand().equals("log"))
+            {
+                textfield.setText("" + Math.log(Double.valueOf(displayText).doubleValue()));
+                
+            }
+            else if (e.getActionCommand().equals("C"))
+            {
+                textfield.setText("");
+            }
+ 
