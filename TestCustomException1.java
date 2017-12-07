4bdfd2e6c0bc4d8759c7b8db77eff787b6366d2f
mittrayash <mittrayash@gmail.com>
2017-12-07 22:51:26 +0530

    Custom Exception Added logic

--- TestCustomException1.java
+++ TestCustomException1.java
@@ -10,3 +10,13 @@ class InvalidAgeException extends Exception{
   super(s);  
  }  
 }
+class TestCustomException1{  
+   static void validate(int age)throws InvalidAgeException{  
+     if(age<18)  
+      throw new InvalidAgeException("not valid");  
+     else  
+      System.out.println("welcome to vote");  
+   }   
+   
+  
+}  
