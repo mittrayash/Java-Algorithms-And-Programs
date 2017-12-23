c20a56441198851f38e7f26815bdc133c416d646
mittrayash <mittrayash@gmail.com>
2017-12-23 21:55:49 +0530

    Optimal binary search tree implementation

---
+++ GFG.java
@@ -0,0 +1,40 @@
+package labwork_ada;
+
+public class GFG 
+{
+    static int optCost(int freq[], int i, int j)
+    {
+       if (j < i)      // no elements in this subarray
+         return 0;
+       if (j == i)     // one element in this subarray
+         return freq[i];
+       int fsum = sum(freq, i, j);
+       int min = Integer.MAX_VALUE;
+       for (int r = i; r <= j; ++r)
+       {
+           int cost = optCost(freq, i, r-1) + 
+                          optCost(freq, r+1, j);
+           if (cost < min)
+              min = cost;
+       }
+       return min + fsum;
+    }
+    static int optimalSearchTree(int keys[], int freq[], int n)
+    {
+         return optCost(freq, 0, n-1);
+    }
+    static int sum(int freq[], int i, int j)
+    {
+        int s = 0;
+        for (int k = i; k <=j; k++)
+           s += freq[k];
+        return s;
+    }
+    public static void main(String[] args) {
+        int keys[] = {10, 12, 20};
+        int freq[] = {34, 8, 50};
+        int n = keys.length;
+        System.out.println("Cost of Optimal BST is " +
+                         optimalSearchTree(keys, freq, n));
+    }
+}
